21a18460777e4b605e6b86ecbf234e3c00fc9888
Arvindder Rajasingam <arvin6730@gmail.com>
2019-06-23 23:37:31 +0800

    First commit initial app

---
+++ src/app/beacon/beacon.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-beacon',
+  templateUrl: './beacon.page.html',
+  styleUrls: ['./beacon.page.scss'],
+})
+export class BeaconPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
